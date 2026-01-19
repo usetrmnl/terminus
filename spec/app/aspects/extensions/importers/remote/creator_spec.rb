7e699bb22f001369196457a149ff82c07b8068e6
Brooke Kuhlmann <brooke@alchemists.io>
2026-01-19 10:39:30 -0700

    Added extension remote import creator
    Necessary create a extension based on TRMNL plugin (recipe) attributes.
    
    Issue: 261
    Milestone: minor

---
+++ spec/app/aspects/extensions/importers/remote/creator_spec.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+require "hanami_helper"
+
+RSpec.describe Terminus::Aspects::Extensions::Importers::Remote::Creator, :db do
+  subject(:processor) { described_class.new transformer: }
+
+  let :transformer do
+    instance_double Terminus::Aspects::Extensions::Importers::Remote::Transformer
+  end
+
+  describe "#call" do
+    let :attributes do
+      {
+        name: "test",
+        label: "Test",
+        description: "Imported from TRMNL.",
+        kind: "poll",
+        poll_headers: nil,
+        fields: [],
+        poll_verb: "get",
+        poll_template: ["https://test.io/test"],
+        interval: 1,
+        unit: "minute",
+        template: "<h1>Test</h1>"
+      }
+    end
+
+    it "imports without associated model" do
+      allow(transformer).to receive(:call).and_return Success(attributes)
+      result = processor.call(1).success.to_h
+
+      expect(result.to_h).to include(name: "test", models: [])
+    end
+
+    it "imports with default model" do
+      model = Factory[:model, name: "og_plus"]
+      allow(transformer).to receive(:call).and_return Success(attributes)
+      result = processor.call(1).success
+
+      expect(result.to_h).to include(name: "test", models: [hash_including(id: model.id)])
+    end
+
+    it "imports with exchanges" do
+      attributes.merge! poll_headers: {accept: "application/json"},
+                        poll_verb: "post",
+                        poll_body: {name: "test"}
+      allow(transformer).to receive(:call).and_return Success(attributes)
+      processor.call(1).success
+
+      data = Terminus::Repositories::ExtensionExchange.new.all.map(&:to_h)
+
+      expect(data).to contain_exactly(
+        hash_including(
+          headers: {"accept" => "application/json"},
+          verb: "post",
+          template: "https://test.io/test",
+          body: {"name" => "test"}
+        )
+      )
+    end
+
+    it "fails with duplicate import" do
+      Factory[:extension, name: "test"]
+      allow(transformer).to receive(:call).and_return Success(attributes)
+
+      expect(processor.call(1)).to be_failure(
+        %(Name must be unique. Please use a value other than "test".)
+      )
+    end
+  end
+end
