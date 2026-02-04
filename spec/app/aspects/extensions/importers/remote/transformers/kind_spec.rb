7e8d6ff893010ca3be18bb46e765da0e7f427f22
Brooke Kuhlmann <brooke@alchemists.io>
2026-02-04 11:43:24 -0700

    Added extension remote import kind transformer
    Necessary to transform the strategy -- and associated data for the strategy -- into kind and body data for import.
    
    Issue: 261
    Milestone: minor

---
+++ spec/app/aspects/extensions/importers/remote/transformers/kind_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require "hanami_helper"
+
+RSpec.describe Terminus::Aspects::Extensions::Importers::Remote::Transformers::Kind do
+  subject(:transformer) { described_class.new }
+
+  describe "#call" do
+    let :attributes do
+      {poll_body: %({"sort":"name"}), static_data: %({"email": "test@test.io"})}
+    end
+
+    it "answers success when polling" do
+      attributes[:strategy] = "polling"
+      expect(transformer.call(attributes)).to be_success(
+        kind: "poll",
+        poll_body: %({"sort":"name"})
+      )
+    end
+
+    it "answers success when static" do
+      attributes[:strategy] = "static"
+
+      expect(transformer.call(attributes)).to be_success(
+        kind: "static",
+        poll_body: %({"email": "test@test.io"})
+      )
+    end
+
+    it "answers failure when webhook" do
+      attributes = {strategy: "webhook"}
+
+      expect(transformer.call(attributes)).to be_failure(
+        "Unsupported kind: webhook. Use: polling or static."
+      )
+    end
+
+    it "answers failure with unknown kind" do
+      attributes = {strategy: "bogus"}
+
+      expect(transformer.call(attributes)).to be_failure(
+        "Unsupported kind: bogus. Use: polling or static."
+      )
+    end
+  end
+end
