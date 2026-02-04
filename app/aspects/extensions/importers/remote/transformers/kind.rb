d65a6dd5e8c1fc6c57875ac1220ce47bc0668f01
Brooke Kuhlmann <brooke@alchemists.io>
2026-02-04 11:43:24 -0700

    Added extension remote import kind transformer
    Necessary to transform the strategy -- and associated data for the strategy -- into kind and body data for import.
    
    Issue: 261
    Milestone: minor

---
+++ app/aspects/extensions/importers/remote/transformers/kind.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require "dry/monads"
+require "refinements/array"
+
+module Terminus
+  module Aspects
+    module Extensions
+      module Importers
+        module Remote
+          module Transformers
+            # Transforms (mutates) strategy and polling/static attributes for initialization.
+            class Kind
+              include Dry::Monads[:result]
+
+              using Refinements::Array
+
+              KINDS = {"polling" => "poll", "static" => "static"}.freeze
+
+              def initialize kinds: KINDS
+                @kinds = kinds
+                @keys = kinds.keys
+              end
+
+              def call attributes
+                strategy = attributes.delete :strategy
+
+                if keys.include? strategy
+                  Success process(strategy, attributes)
+                else
+                  Failure "Unsupported kind: #{strategy}. Use: #{keys.to_sentence :or}."
+                end
+              end
+
+              private
+
+              attr_reader :kinds, :keys
+
+              # :reek:FeatureEnvy
+              def process strategy, attributes
+                kind = kinds[strategy]
+                static_data = attributes.delete :static_data
+
+                if kind == "static"
+                  attributes.merge! kind:, body: static_data
+                else
+                  attributes.merge! kind:, poll_body: attributes[:poll_body]
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
