c49f7753c4d71e8a9e14ae047fce3ec0434a931a
Brooke Kuhlmann <brooke@alchemists.io>
2026-04-06 16:30:42 -0600

    Updated extension edit form to link to exchanges
    Necessary to use these new exchanges instead of the original headers, verb, uri, and body so we can support multiple exhanges all with their own unique attributes.
    
    Issue: 261
    Milestone: minor

--- app/views/extensions/edit.rb
+++ app/views/extensions/edit.rb
@@ -7,12 +7,14 @@ module Terminus
       class Edit < View
         include Deps[
           model_repository: "repositories.model",
-          device_repository: "repositories.device"
+          device_repository: "repositories.device",
+          exchange_repository: "repositories.extension_exchange"
         ]
 
         expose(:default_model) { model_repository.find_by name: "og_plus" }
         expose(:models) { model_repository.all.map { [it.label, it.id] } }
         expose(:devices) { device_repository.all.map { [it.label, it.id] } }
+        expose(:exchanges) { |extension:| exchange_repository.where extension_id: extension.id }
         expose :extension
         expose :fields, default: Dry::Core::EMPTY_HASH
         expose :errors, default: Dry::Core::EMPTY_HASH
