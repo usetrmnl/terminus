e94435b9c321debfb4e73f5fe5cbd28c96913550
Brooke Kuhlmann <brooke@alchemists.io>
2026-04-19 07:31:24 -0600

    Updated playlist screen show view to use multiple actions
    Instead of only being able to navigate to the previous and last screen, you can now jump to the first and last screen as well. This is done by having the show view compute the URIs for first, previous, next, and last links (this is also why the routes are injected). Doing this vastly simplifies the URI logic so we can compute this once via the view and then reuse throughout the partial.
    
    The `screen` partial makes use of the recently added `direction` partial to cut down on the boilerplate code as well.
    
    Finally, the status for knowing which screen out of the total screens is calculated once and then reused for the progress element aria label and also shown visibly within the actions area to make all of this more clear on where you are in the slide show timeline.
    
    Milestone: minor

--- app/views/playlists/screens/show.rb
+++ app/views/playlists/screens/show.rb
@@ -6,13 +6,39 @@ module Terminus
       module Screens
         # The show view.
         class Show < View
+          include Deps[:routes]
+
           expose :playlist
-          expose :before
           expose :current
-          expose :after
-          expose(:index) { |playlist:, current:| playlist.screens.index current }
-          expose(:total) { |playlist:| playlist.screens.size - 1 }
-          expose(:status) { |index, total| "Slide #{index + 1} of #{total + 1}" if index && total }
+          expose(:index, decorate: false) { |screens, current:| screens.index current }
+          expose(:total, decorate: false) { |screens| screens.size - 1 }
+
+          expose :status, decorate: false do |index, total|
+            "#{index + 1} of #{total + 1}" if index && total
+          end
+
+          expose :previous_uri, decorate: false do |playlist:, before:|
+            routes.path :playlist_screen, playlist_id: playlist.id, id: before.id if before
+          end
+
+          expose :next_uri, decorate: false do |playlist:, after:|
+            routes.path :playlist_screen, playlist_id: playlist.id, id: after.id if after
+          end
+
+          expose :first_uri, decorate: false do |screens, playlist:|
+            first = screens.first
+            routes.path :playlist_screen, playlist_id: playlist.id, id: first.id if first
+          end
+
+          expose :last_uri, decorate: false do |screens, playlist:|
+            last = screens.last
+            routes.path :playlist_screen, playlist_id: playlist.id, id: last.id if last
+          end
+
+          private_expose :routes
+          private_expose :before, decorate: false
+          private_expose :after, decorate: false
+          private_expose(:screens, decorate: false) { |playlist:| playlist.screens }
         end
       end
     end
