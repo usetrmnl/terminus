d3f521a26407ea6b425e6f7cbf4a1da79d88fba3
Brooke Kuhlmann <brooke@alchemists.io>
2026-04-26 09:18:24 -0600

    Added extension exporter
    Necessary to export existing extensions for import into other Terminus instances.
    
    Milestone: minor

---
+++ app/aspects/extensions/exporter.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "dry/monads"
+
+module Terminus
+  module Aspects
+    module Extensions
+      # Exports an extension for sharing elsewhere.
+      class Exporter
+        include Deps[
+          extension_repository: "repositories.extension",
+          exchange_repository: "repositories.extension_exchange"
+        ]
+        include Dry::Monads[:result]
+
+        def call id
+          extension = extension_repository.find id
+
+          return Failure "Unable to find extension: #{id}." unless extension
+
+          Success extension.export_attributes
+        end
+      end
+    end
+  end
+end
