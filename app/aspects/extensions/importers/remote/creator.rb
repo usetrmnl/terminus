cf7c7f9648cc01acb6f98a5c6224c5facfbd516a
Brooke Kuhlmann <brooke@alchemists.io>
2026-06-30 14:45:56 -0600

    Refactored extention remote importers to use reqliquefier
    Reduces duplication by replacing the original template keys tranformer with the newer reliquefier.
    
    Milestone: patch

--- app/aspects/extensions/importers/remote/creator.rb
+++ app/aspects/extensions/importers/remote/creator.rb
@@ -14,7 +14,7 @@ module Terminus
               :logger,
               :i18n,
               "aspects.extensions.importers.remote.transformer",
-              keyer: "aspects.extensions.importers.remote.transformers.template_keys",
+              reliquefier: "aspects.extensions.importers.remote.transforms.reliquefier",
               repository: "repositories.extension",
               model_repository: "repositories.model",
               exchange_repository: "repositories.extension_exchange"
@@ -66,7 +66,7 @@ module Terminus
             end
 
             def transform_exchange_template content
-              case keyer.call content
+              case reliquefier.call content
                 in Success(content) then content
                 in Failure(message)
                   logger.debug { message }
