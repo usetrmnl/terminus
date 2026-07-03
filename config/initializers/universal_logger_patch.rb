7ee778570b5d27e81e02d949b54585b7fa9617d3
Brooke Kuhlmann <brooke@alchemists.io>
2026-07-03 16:41:10 -0600

    Fixed Hanami universal logger patch to handle Rack and SQL entries
    Now that we are upgraded to Cogger 2.5.0 and the logger provider registers Rack and SQL formatters, we can detect incoming messages/paylaods and dynamically switch formatters based on tag information.
    
    Milestone: patch

--- config/initializers/universal_logger_patch.rb
+++ config/initializers/universal_logger_patch.rb
@@ -1,22 +1,24 @@
 # auto_register: false
 # frozen_string_literal: true
 
-# Patches Hanami's universal logger.
+# Patches Hanami's universal logger to work with Cogger.
 module UniversalLoggerPatch
   def _log_structured method, message, payload
-    tags = Array _current_tags
+    tags = _current_tags
+    block_content = yield if block_given?
 
-    return logger.public_send(method, message, tags:, **payload) unless block_given?
+    logger.formatter = case tags
+                         in [:rack] then tags.delete :rack
+                         in [:sql] if block_content.is_a? Hash then tags.delete :sql
+                         else Hanami.env == :development ? :emoji : :json
+                       end
 
-    block_content = yield
-
-    if block_content.is_a?(Hash) && block_content.key?(:db)
-      tags.append block_content
-    else
+    unless block_content.is_a? Hash
       message = block_content
+      block_content = {}
     end
 
-    logger.public_send method, message, tags:, **payload
+    logger.public_send method, message, tags:, **block_content.merge!(payload)
   end
 end
 
