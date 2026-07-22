f50cd810c014fbacf762b794fe9f150fd52a3261
Brooke Kuhlmann <brooke@alchemists.io>
2026-07-22 11:45:37 -0600

    Updated device factory to provide default command
    Necessary to match database behavior.
    
    Issue: 345
    Milestone: minor

--- spec/support/factories/device.rb
+++ spec/support/factories/device.rb
@@ -12,6 +12,7 @@ Factory.define :device, relation: :device do |factory|
   factory.firmware_version "1.2.3"
   factory.label "Test"
   factory.mac_address "A1:B2:C3:D4:E5:F6"
+  factory.command "none"
   factory.touch_bar "tap"
   factory.wifi_band 0
   factory.wifi_signal(-44)
