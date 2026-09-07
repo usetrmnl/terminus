0a64b17a9ec4e4268fe8cb555f9801e561322181
Brooke Kuhlmann <brooke@alchemists.io>
2026-09-07 13:26:26 -0600

    Added Extensions API delete action
    Necessary to delete an existing extension.
    
    Milestone: minor

---
+++ app/actions/api/extensions/delete.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Terminus
+  module Actions
+    module API
+      module Extensions
+        # The delete action.
+        class Delete < Base
+          include Deps["aspects.extensions.deleter"]
+          include Initable[serializer: Serializers::Extension]
+
+          using Refines::Actions::Response
+
+          def handle request, response
+            case deleter.call request.params.to_h
+              in Success(extension) then success extension, response
+              else failure response
+            end
+          end
+
+          private
+
+          def success extension, response
+            response.with body: {data: serializer.new(extension).to_h}.to_json
+          end
+
+          def failure(response) = response.with_details problem[status: :not_found]
+        end
+      end
+    end
+  end
+end
