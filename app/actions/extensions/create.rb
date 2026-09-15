4ad31ffb4a56abee6c8d356f0987463945957c47
Brooke Kuhlmann <brooke@alchemists.io>
2026-09-15 11:08:15 -0600

    Refactored extensions create action to use creator
    Necessary to reduce duplication by delegating to the creator which now encapsulates this logic.
    
    Milestone: patch

--- app/actions/extensions/create.rb
+++ app/actions/extensions/create.rb
@@ -9,45 +9,33 @@ module Terminus
       class Create < Action
         include Deps[
           :htmx_layout,
-          "aspects.jobs.schedule",
+          "aspects.extensions.creator",
+          validator: "contracts.extensions.create",
           repository: "repositories.extension",
           index_view: "views.extensions.index"
         ]
 
         using Refinements::Hash
 
-        contract Contracts::Extensions::Create
-
         def handle request, response
-          parameters = request.params
-
-          if parameters.valid?
-            save parameters
-            response.render index_view,
-                            extensions: repository.all,
-                            layout: htmx_layout.call(request)
-          else
-            error response, parameters
+          case creator.call(request.params.to_h, validator:)
+            in Success then success request, response
+            in Failure(result) then failure result, response
           end
         end
 
         private
 
-        def save parameters
-          attributes = parameters[:extension]
-          model_ids, device_ids = parameters.to_h.values_at :model_ids, :device_ids
-          extension = repository.create_with_models attributes, Array(model_ids)
-
-          repository.update_with_devices extension.id, {}, Array(device_ids)
-          schedule.upsert(*extension.to_schedule)
+        def success request, response
+          response.render index_view, extensions: repository.all, layout: htmx_layout.call(request)
         end
 
-        def error response, parameters
-          fields = parameters[:extension].transform_with!(
+        def failure result, response
+          fields = result[:extension].transform_with!(
             start_at: -> value { value.strftime("%Y-%m-%dT%H:%M:%S") }
           )
 
-          response.render view, fields:, errors: parameters.errors[:extension], layout: false
+          response.render view, fields:, errors: result.errors[:extension], layout: false
         end
       end
     end
