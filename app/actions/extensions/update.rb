56bc9e4b2019b6be9c37b0a75898e734dde31bdc
Brooke Kuhlmann <brooke@alchemists.io>
2026-09-15 11:38:32 -0600

    Refactored extensions update action to use updater
    Necessary to reduce duplication by delegating to the updater which encapsulates this logic.
    
    Milestone: patch

--- app/actions/extensions/update.rb
+++ app/actions/extensions/update.rb
@@ -8,49 +8,31 @@ module Terminus
       # The update action.
       class Update < Action
         include Deps[
-          "aspects.jobs.schedule",
           "aspects.errors.detailer",
-          repository: "repositories.extension"
+          "aspects.extensions.updater",
+          validator: "contracts.extensions.update"
         ]
 
         using Refinements::Hash
 
-        contract Contracts::Extensions::Update
-
         def handle request, response
-          parameters = request.params
-          extension = repository.find parameters[:id]
-
-          halt :unprocessable_content unless extension
-
-          if parameters.valid?
-            render extension, parameters, response
-          else
-            error extension, parameters, response
+          case updater.call(request.params.to_h, validator:)
+            in Success(extension) then success extension, response
+            in Failure(String) then halt :unprocessable_content
+            in Failure(*tuple) then failure(*tuple, response)
           end
         end
 
         private
 
-        def render extension, parameters, response
-          update extension, parameters
-
+        def success extension, response
           response.flash[:notice] = "Changes saved."
           response.redirect_to routes.path(:extension_edit, id: extension.id)
         end
 
-        def update extension, parameters
-          id = extension.id
-          model_ids, device_ids = parameters.to_h.values_at :model_ids, :device_ids
-          extension = repository.update_with_models id, parameters[:extension], Array(model_ids)
-
-          repository.update_with_devices id, {}, Array(device_ids)
-          schedule.upsert(*extension.to_schedule, old_name: extension.screen_name)
-        end
-
-        def error extension, parameters, response
-          errors = parameters.errors[:extension]
-          fields = parameters[:extension].transform_with!(
+        def failure extension, result, response
+          errors = result.errors[:extension]
+          fields = result[:extension].transform_with!(
             start_at: -> value { value.strftime("%Y-%m-%dT%H:%M:%S") }
           )
 
