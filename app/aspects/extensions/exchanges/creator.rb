1cbb755902cc0e1175805ff62d78913d8936b097
Brooke Kuhlmann <brooke@alchemists.io>
2026-09-21 14:20:40 -0600

    Added extension exchange creator
    Necessary to encapsulate the creation of a new exchange. Will soon be used for UI and API purposes.
    
    Milestone: minor

---
+++ app/aspects/extensions/exchanges/creator.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "core"
+require "initable"
+
+module Terminus
+  module Aspects
+    module Extensions
+      module Exchanges
+        # Creates a new exchange.
+        class Creator
+          include Deps["aspects.jobs.schedule", repository: "repositories.extension_exchange"]
+          include Initable[job: Terminus::Jobs::Extensions::ExchangeRefresh]
+
+          def call attributes, validator:
+            validator.call(attributes).to_monad.fmap { create it.to_h }
+          end
+
+          private
+
+          def create attributes
+            exchange = repository.create extension_id: attributes[:extension_id],
+                                         **attributes[:exchange]
+
+            job.perform_async exchange.id
+            exchange
+          end
+        end
+      end
+    end
+  end
+end
