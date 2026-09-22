815180a2922082edbed409b575daeb60f7b629b3
Brooke Kuhlmann <brooke@alchemists.io>
2026-09-22 10:41:09 -0600

    Added extension exchange create schema
    Necessary to create an extension exchange. Will soon be used by the API create action.
    
    Milestone: minor

---
+++ app/schemas/extensions/exchanges/create.rb
@@ -0,0 +1,18 @@
+# auto_register: false
+# frozen_string_literal: true
+
+module Terminus
+  module Schemas
+    module Extensions
+      module Exchanges
+        # Defines extension exchange create schema.
+        Create = Dry::Schema.Params do
+          optional(:headers).maybe :hash
+          optional(:verb).filled :string
+          required(:template).filled :string
+          optional(:body).maybe :hash
+        end
+      end
+    end
+  end
+end
